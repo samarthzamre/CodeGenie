22d6b4be75b58610edbef6a6eb98d6b0a7af031c
samarthzamre <samarthzamre891@gmail.com>
2025-07-17 18:23:06 +0530

    🔧 Fix Cross-Origin-Opener-Policy header for Google OAuth

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:['lh3.googleusercontent.com'],
-  },  
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
